18e33a8ffd6847e6e97724936a601fd6f94c171f
YassineBahadi <yassinebahadi04@gmail.com>
2026-03-07 15:59:02 +0000

    first commit

---
+++ src/main/java/ma/yassine/activitepratique2/Activitepratique2Application.java
@@ -0,0 +1,13 @@
+package ma.yassine.activitepratique2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activitepratique2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Activitepratique2Application.class, args);
+	}
+
+}
