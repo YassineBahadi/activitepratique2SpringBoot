26770c53823a9b115ae27407d6a4ad513853be30
YassineBahadi <yassinebahadi04@gmail.com>
2026-03-07 22:44:39 +0000

    feat(product): add product creation form and save functionality

--- src/main/java/ma/yassine/activitepratique2/web/ProductController.java
+++ src/main/java/ma/yassine/activitepratique2/web/ProductController.java
@@ -1,9 +1,11 @@
 package ma.yassine.activitepratique2.web;
 
+import ma.yassine.activitepratique2.entities.Product;
 import ma.yassine.activitepratique2.repositories.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @author pc
@@ -26,6 +28,18 @@ public class ProductController {
     @GetMapping("/delete")
     public String deleteProduct(Long id){
         productRepository.deleteById(id);
+        return "redirect:/index";
+    }
+
+    @GetMapping("/formProducts")
+    public String formProducts(Model model){
+        model.addAttribute("product",new Product());
+        return "formProducts";
+    }
+
+    @PostMapping("/save")
+    public String save(Product product){
+        productRepository.save(product);
         return "redirect:/index";
     }
 }
