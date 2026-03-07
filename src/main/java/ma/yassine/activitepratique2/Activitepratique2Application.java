33a66088d142988c51b4d9cb0c83dc5b65a581f3
YassineBahadi <yassinebahadi04@gmail.com>
2026-03-07 16:14:48 +0000

    test(dao): test ProductRepository with CommandLineRunner

--- src/main/java/ma/yassine/activitepratique2/Activitepratique2Application.java
+++ src/main/java/ma/yassine/activitepratique2/Activitepratique2Application.java
@@ -1,7 +1,11 @@
 package ma.yassine.activitepratique2;
 
+import ma.yassine.activitepratique2.entities.Product;
+import ma.yassine.activitepratique2.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Activitepratique2Application {
@@ -10,4 +14,16 @@ public class Activitepratique2Application {
 		SpringApplication.run(Activitepratique2Application.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ProductRepository productRepository){
+		return args->{
+			productRepository.save(new Product(null,"Computer",9000,3));
+			productRepository.save(new Product(null,"Printer",1200,2));
+			productRepository.save(new Product(null,"SmartPhone",5000,10));
+
+			productRepository.findAll().forEach(p->{
+				System.out.println(p.getName());
+			});
+		};
+	}
 }
